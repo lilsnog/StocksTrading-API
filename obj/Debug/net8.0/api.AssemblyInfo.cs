dddda2d69308e01d43850862b2ec6c0e0682aefa
Godswill Sam-Nwafor <godswill.sam-nwafor@greenwichbankgroup.com>
2024-07-30 11:10:41 +0100

    Repository Pattern + Dependency Injectio

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3cecedcc7f1c5bebe52b0fc3a657f70610816c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62aad97bb12ec25a919621bf8733bfde76ba4dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
